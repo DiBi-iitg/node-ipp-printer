44566bee7b6dafb491d47c189b817fa4fc9fd40a
Ankit D <ankit.99d@gmail.com>
2023-02-03 10:30:34 +0530

    upload to server

--- handleParseResponse.js
+++ handleParseResponse.js
@@ -1,6 +1,8 @@
 import * as fsExtra from "fs-extra";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
-const handleParse = (response) => {
+const handleParse = async (response) => {
 	const prediction = response?.document?.inference?.prediction;
 	if (!prediction) {
 		console.log("Error parsing");
@@ -8,33 +10,35 @@ const handleParse = (response) => {
 	}
 	// console.log(prediction["customer_address"]);
 	// console.log(prediction["customer_company_registrations"]);
-	console.log(prediction["customer_name"]);
+	let consumerDetail = prediction["customer_name"].value.toString();
+	let vendorId = prediction["invoice_number"].value.toString();
+	let totalAmount = prediction["total_amount"].value.toString();
 	let txns = [];
+
 	prediction["line_items"].forEach((item) => {
 		txns.push({
-			id: "id",
+			id: uuidv4(),
 			itemName: item.description ? item.description.toString() : "not-present",
 			itemQty: item.quantity ? item.quantity.toString() : "not-present",
 			itemPrice: item.unit_price ? item.unit_price.toString() : "not-present",
 			itemTotal: item.total_amount ? item.total_amount.toString() : "not-present",
 		});
 	});
-	console.log(txns);
-	// prediction["line_items"].forEach((item) => {
-	// 	console.log("-----------------------------");
-	// 	console.log(
-	// 		"Item:",
-	// 		item.description,
-	// 		"\n",
-	// 		"Qty:",
-	// 		item.quantity ? item.quantity : "not-in-invoice",
-	// 		"\n",
-	// 		"Total",
-	// 		item.total_amount,
-	// 		"\n"
-	// 	);
-	// 	console.log("-----------------------------");
-	// });
+	let invoiceNumber = uuidv4();
+	console.log("Uploading data...");
+	try {
+		const resp = await axios.post("http://localhost:8080/api/transaction", {
+			vendorId,
+			consumerDetail,
+			invoiceNumber,
+			txns,
+			totalAmount,
+		});
+		console.log(resp.data);
+		console.log("Uploading done!");
+	} catch (error) {
+		console.log("Error uploading!");
+	}
 	console.log("Cleaning files...");
 	try {
 		fsExtra.emptyDirSync("./compressed");
